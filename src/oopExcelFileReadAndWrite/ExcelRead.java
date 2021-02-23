b28bf200ae49419eead4acb72f37ab9932984895
arafat147 <arafat.rahman93@gmail.com>
2021-02-22 23:21:56 -0500

    excel read

---
+++ src/oopExcelFileReadAndWrite/ExcelRead.java
@@ -0,0 +1,16 @@
+package oopExcelFileReadAndWrite;
+
+import java.io.File;
+
+public class ExcelRead {
+
+    public static void main(String[] args) {
+
+        File fl = new File("C:\\Users\\arafa\\eclipse-workspace\\Java_Notes_And_Practice\\src\\Book1.xlsx");
+
+
+
+    }
+
+
+}
